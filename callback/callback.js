3b9cb00fc1fcb89dee6cbc348d22efef7fd55305
seungdang123 <seungdang123@gmail.com>
2021-12-29 04:27:19 +0900

    Update callback.js

--- callback/callback.js
+++ callback/callback.js
@@ -26,3 +26,54 @@ function printWithDelay(print, timeout) {
 }
 
 printWithDelay(() => console.log("async callback"), 2000);
+
+
+
+
+
+// Callback Hell example
+class UserStorage {
+    loginUser(id, pw, onSuccess, onError) {
+        setTimeout(() => {
+            if (
+                (id === 'seung' && pw === "hwan") ||
+                (id === 'chicken' && pw === "beer")
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        }, 2000);
+    }
+
+    getRoles(user, onSuccess, onError) {
+        setTimeout(() => {
+            if (user === 'seung') {
+                onSuccess({ name: 'seung' , role: 'admin'});
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const pw = prompt('enter your password');
+userStorage.loginUser(
+    id,
+    pw,
+    (user) => {
+        userStorage.getRoles(
+            user, 
+            (userWithRole) => {
+                alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+            },
+            (error) => {
+                console.log(error)
+            }
+            );
+    },
+    (error) => {console.log(error)}
+    )
